64cfbbbbccf85ce60f0efb43b3f4179e3d510e02
camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2017-05-02 16:25:04 -0500

    added tasks 3a and 3b

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Codigo: 1324486
 
 Ejercicios de la clase 05-02-2017 - Plataformas a Gran Escala
+
+Informe: https://docs.google.com/a/correounivalle.edu.co/document/d/1wAUx-uNzeLKlAsj_mh_6QzmdjM67CXgPygONuaRftHo/edit?usp=sharing
---
+++ mpiEx3b.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc,char *argv[]){
+	int size, rank, dest, source, count, tag=1;
+	int inmsg, outmsg=5432;
+	MPI_Status Stat;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	dest = (rank + 1) % size;
+
+	if (rank == 0) {
+	  source = size - 1;  
+	  MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+	  MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+	}else {
+	  source = rank - 1;
+	  MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+	  MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+	}
+
+	//MPI_Get_count(&Stat, MPI_INT, &count);
+	printf("Task %d: Received %d (integer) from task %d with tag %d \n",
+		   rank, inmsg, Stat.MPI_SOURCE, Stat.MPI_TAG);
+
+	MPI_Finalize();
+}
